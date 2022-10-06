5b08b3a1bad510bfa4a1825b36601f7a5877a85f
GNIFING <narawich.fing@gmail.com>
2022-10-06 15:11:59 +0700

    Fing-development

--- src/routes/RootNavigation.tsx
+++ src/routes/RootNavigation.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CreateDatasetLanding from "../pages/createDataset/CreateDatasetLanding";
-import Landing from "../pages/Landing";
+import HomeScreen from "../pages/homeScreen/HomeScreen";
+import Landing from "../pages/landing/Landing";
 
 const RootNavigation = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/create-dataset" element={<CreateDatasetLanding />} />
-        <Route path="/" element={<Landing />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/" element={<HomeScreen />} />
       </Routes>
     </BrowserRouter>
   );
